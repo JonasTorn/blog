1636e38a7a4c79ae2249478cf51d6887f31a5f8b
Igelkotte <jonas.toern@gmail.com>
2025-01-06 16:02:18 +0100

    Added page components and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PostsPageComponent } from './components/posts-page/posts-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent }, 
+    { path: 'posts', component: PostsPageComponent},
+    { path: 'about', component: AboutPageComponent}
+];
