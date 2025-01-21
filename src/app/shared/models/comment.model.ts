0c088a4bd40b6d22ffcb3ef12e23b68f858288e3
Igelkotte <jonas.toern@gmail.com>
2025-01-21 19:25:06 +0100

    formatted comment.model

--- src/app/shared/models/comment.model.ts
+++ src/app/shared/models/comment.model.ts
@@ -3,7 +3,11 @@ export class BlogPostComment {
 	content: string;
 	date: number;
 
-	constructor(author: string, content: string, date: number) {
+	constructor(
+        author: string,
+        content: string,
+        date: number
+        ) {
 		this.author = author;
 		this.content = content;
 		this.date = date;
