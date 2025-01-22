93e9d6c35a2f096a2f87a44e8dbd2cf16f4c97fe
Igelkotte <jonas.toern@gmail.com>
2025-01-22 12:29:24 +0100

    New Favicon. Center navbar round "about". Fixed mock data comments dates

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,10 +4,11 @@ import { AdminToggleComponent } from "../admin-toggle/admin-toggle.component";
 import { ThemeToggleComponent } from "../theme-toggle/theme-toggle.component";
 import { Divider } from "primeng/divider";
 import { AdminService } from "../../core/services/admin.service";
+import { Button } from "primeng/button";
 
 @Component({
 	selector: "app-header",
-	imports: [RouterLink, AdminToggleComponent, ThemeToggleComponent, Divider],
+	imports: [RouterLink, AdminToggleComponent, ThemeToggleComponent, Divider, Button],
 	templateUrl: "./header.component.html",
 	styleUrl: "./header.component.css",
 })
--- src/app/shared/mock-data/BLOG_POSTS.ts
+++ src/app/shared/mock-data/BLOG_POSTS.ts
@@ -18,15 +18,18 @@ export const BLOG_POSTS: BlogPost[] = [
 			new BlogPostComment(
 				"Anonymous",
 				"Testar en lång kommentar %%#)#% Cras eros orci, accumsan eget ante vel, pharetra vehicula enim. Integer mollis fermentum ex sit amet posuere. Morbi a condimentum ex. Sed risus ante, scelerisque quis luctus vel, ullamcorper at est. Nam id leo vestibulum, hendrerit dolor eu, maximus diam. Fusce urna est, convallis laoreet posuere id, rhoncus at ipsum. Sed ante metus, viverra in orci ac, hendrerit scelerisque magna. Ut a tellus non magna fringilla lobortis a vel odio. In eu feugiat lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam tortor felis, non vestibulum erat dignissim a. Donec at fringilla metus. Curabitur et nunc at eros commodo volutpat. Pellentesque faucibus eleifend nisi, nec sagittis purus semper a. In viverra scelerisque arcu, ac pharetra augue feugiat eu. Aliquam posuere urna nec massa sollicitudin viverra. Curabitur tempor ligula at erat auctor interdum. Donec in mi venenatis, ultrices magna ac, interdum ipsum. Sed volutpat leo a nisi aliquet, et mattis massa luctus. Nunc sit amet sagittis augue. Cras at vestibulum ipsum. Ut maximus elementum eros, et vulputate tortor blandit a. Nam molestie dignissim mattis. Donec sit amet dignissim mi, vitae pellentesque ante. Aliquam id odio eget ante dapibus sagittis eu id justo. Sed ac urna blandit, blandit nibh ac, elementum nisl. In at semper orci, mattis fringilla nunc. Mauris diam sem, tempor nec ex ut, pellentesque tempus erat. Nunc ornare lacus tortor, eget consequat arcu efficitur ac. In vel rhoncus purus. Maecenas vel felis non tortor interdum hendrerit.",
-				new Date("2025-01-19").getTime()
+				new Date("2024-07-19").getTime()
 			),
 			new BlogPostComment(
 				"TEST",
 				"Testar spam: ASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSF",
-				new Date("2025-06-27").getTime()
+				new Date("2024-07-27").getTime()
 			),
-            new BlogPostComment("William", "Shit vad bra du är på att koda!", new Date("2025-06-22").getTime()),
-
+			new BlogPostComment(
+				"William",
+				"Shit vad bra du är på att koda!",
+				new Date("2025-01-22").getTime()
+			),
 		],
 		4,
 		false
@@ -37,17 +40,17 @@ export const BLOG_POSTS: BlogPost[] = [
 		"/images/DSC00083.jpg",
 		"Integer rutrum enim eget accumsan tristique. Cras eros orci, accumsan eget ante vel, pharetra vehicula enim. Integer mollis fermentum ex sit amet posuere. Morbi a condimentum ex. Sed risus ante, scelerisque quis luctus vel, ullamcorper at est. Nam id leo vestibulum, hendrerit dolor eu, maximus diam. Fusce urna est, convallis laoreet posuere id, rhoncus at ipsum. Sed ante metus, viverra in orci ac, hendrerit scelerisque magna. Ut a tellus non magna fringilla lobortis a vel odio. In eu feugiat lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam tortor felis, non vestibulum erat dignissim a. Donec at fringilla metus. Curabitur et nunc at eros commodo volutpat. Pellentesque faucibus eleifend nisi, nec sagittis purus semper a. In viverra scelerisque arcu, ac pharetra augue feugiat eu. Aliquam posuere urna nec massa sollicitudin viverra. Curabitur tempor ligula at erat auctor interdum. Donec in mi venenatis, ultrices magna ac, interdum ipsum. Sed volutpat leo a nisi aliquet, et mattis massa luctus. Nunc sit amet sagittis augue. Cras at vestibulum ipsum. Ut maximus elementum eros, et vulputate tortor blandit a. Nam molestie dignissim mattis. Donec sit amet dignissim mi, vitae pellentesque ante. Aliquam id odio eget ante dapibus sagittis eu id justo. Sed ac urna blandit, blandit nibh ac, elementum nisl. In at semper orci, mattis fringilla nunc. Mauris diam sem, tempor nec ex ut, pellentesque tempus erat. Nunc ornare lacus tortor, eget consequat arcu efficitur ac. In vel rhoncus purus. Maecenas vel felis non tortor interdum hendrerit.",
 		"Jonas",
-		new Date("2024-06-25").getTime(),
+		new Date("2024-06-24").getTime(),
 		[
 			new BlogPostComment(
 				"Johan",
 				"Intressant! Kommer att följa :)",
-				new Date("2025-06-22").getTime()
+				new Date("2024-06-25").getTime()
 			),
 			new BlogPostComment(
 				"Anonymous",
 				"Kul!",
-				new Date("2025-06-22").getTime()
+				new Date("2024-06-26").getTime()
 			),
 		],
 		15,
@@ -61,11 +64,17 @@ export const BLOG_POSTS: BlogPost[] = [
 		"Erik",
 		new Date("2024-11-02").getTime(),
 		[
-			new BlogPostComment("", "Snygg bild!", new Date("2025-06-22").getTime()),
+			new BlogPostComment(
+				"Frenka",
+				"Det var verkligen blåsigt!",
+				new Date("2024-11-10").getTime()
+			),
+
+			new BlogPostComment("", "Snygg bild!", new Date("2024-11-10").getTime()),
 			new BlogPostComment(
 				"TEST",
 				"Testar spam: ASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSFASFLKANSFLKASFLBASFLKABSF",
-				new Date("2025-06-27").getTime()
+				new Date("2024-11-03").getTime()
 			),
 		],
 		25,
